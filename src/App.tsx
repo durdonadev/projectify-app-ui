1adb78ca84c4f94247796f3ffec8fa9ae08a4a8a
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-29 22:10:23 -0500

    Create logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
-import { Button, Typography, Input, Modal, Avatar } from "./design-system";
-import john from "./assets/image.png";
+import {
+    Button,
+    Typography,
+    Input,
+    Modal,
+    Avatar,
+    Logo
+} from "./design-system";
 
 const App = () => {
-    return (
-        <div style={{ padding: "100px" }}>
-            <Avatar type="initials" size="lg" shape="circle">
-                John Doe
-            </Avatar>
-            <Avatar type="photo" size="lg" shape="rounded" imageUrl={john}>
-                John Doe
-            </Avatar>
-            <Avatar type="photo" size="lg" shape="circle" imageUrl={john}>
-                John Doe
-            </Avatar>
-        </div>
-    );
+    return <div style={{ padding: "100px" }}></div>;
 };
 
 export { App };
