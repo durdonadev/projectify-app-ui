600015a760fc59750003b37d9232693a7330d82a
Durdona Ibragimova <durdona.dev@gmail.com>
2024-01-07 00:55:47 -0500

    Refactor Avatar component

--- src/design-system/Avatar/Avatar.tsx
+++ src/design-system/Avatar/Avatar.tsx
@@ -1,43 +1,52 @@
-import React, { FC } from "react";
-import { trimWhiteSpaces, getNameInitials } from "../utils";
-import { sizeClassNames, shapeClassNames } from "./classnames";
+import React from "react";
 import "./Avatar.css";
-
-type AvatarSize = "sm" | "md" | "lg";
-type AvatarShape = "rounded" | "circle";
-type AvatarType = "initials" | "photo";
+import { trimWhiteSpaces } from "../utils";
 
 type AvatarProps = {
-    size?: AvatarSize;
-    shape?: AvatarShape;
-    type?: AvatarType;
-    className?: string;
-    children: React.ReactNode;
-    onClick?: () => void;
+    firstName: string;
+    lastName: string;
+    shape?: "rounded" | "circle";
+    size?: "sm" | "md" | "lg";
     imageUrl?: string;
+    className?: string;
+    onClick?: () => void;
 };
 
-const Avatar: FC<AvatarProps> = (props) => {
-    const { size, shape, type, className, children, onClick, imageUrl } = props;
+const sizeClassNames = {
+    sm: "avatar-small",
+    md: "avatar-medium",
+    lg: "avatar-large"
+};
 
-    const sizeClassName = size ? sizeClassNames[size] : "";
-    const shapeClassName = shape ? shapeClassNames[shape] : "";
-    let typeElement: React.ReactNode = children;
+const shapeClassNames = {
+    rounded: "avatar-rounded",
+    circle: "avatar-circle"
+};
 
-    if (type === "initials" && typeof children === "string") {
-        const initials = getNameInitials(children);
-        typeElement = <span>{initials}</span>;
-    } else if (type === "photo" && imageUrl) {
-        typeElement = <img src={imageUrl} alt="Avatar" />;
-    }
-
-    const finalClassNames = `avatar ${sizeClassName} ${shapeClassName} ${
+const Avatar: React.FC<AvatarProps> = ({
+    firstName,
+    lastName,
+    shape,
+    size,
+    imageUrl,
+    onClick,
+    className
+}) => {
+    const sizeClassName = size !== undefined ? sizeClassNames[size] : "";
+    const shapeClassName = shape !== undefined ? shapeClassNames[shape] : "";
+    const finalClassNames = `avatar ${sizeClassName} ${shapeClassName} $ ${
         className || ""
     }`;
 
     return (
         <button className={trimWhiteSpaces(finalClassNames)} onClick={onClick}>
-            {typeElement}
+            {imageUrl ? (
+                <img src={imageUrl} alt={`${firstName} ${lastName}`} />
+            ) : (
+                `${firstName ? firstName[0].toUpperCase() : ""}${
+                    lastName ? lastName[0].toUpperCase() : ""
+                }`
+            )}
         </button>
     );
 };
