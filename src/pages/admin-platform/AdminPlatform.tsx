d2f8ef7153281ecd4875b3bd14a87729bee5b25d
Durdona Ibragimova <durdona.dev@gmail.com>
2024-01-07 02:39:27 -0500

    Add Side bar user

--- src/pages/admin-platform/AdminPlatform.tsx
+++ src/pages/admin-platform/AdminPlatform.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { SideBar, SideBarLinks } from "../../design-system";
-import { AppContent, AppLayout, User } from "../components";
+import { AppContent, AppLayout, SideBarUser } from "../components";
+import user from "../../assets/images/user.jpg";
 
 const links = [
     {
@@ -49,7 +50,14 @@ const AdminPlatform = () => {
     return (
         <AppLayout>
             <SideBar>
-                <User />
+                <SideBarUser
+                    details={{
+                        firstName: "Aisha",
+                        lastName: "Farah",
+                        imageUrl: user,
+                        email: "info@email.com"
+                    }}
+                />
                 <SideBarLinks links={links} />
             </SideBar>
             <AppContent>
--- src/pages/components/index.ts
+++ src/pages/components/index.ts
@@ -2,3 +2,4 @@ export * from "./AuthWrapper";
 export * from "./PasswordWrapper";
 export * from "./AppLayout";
 export * from "./User";
+export * from "./SideBarUser";
