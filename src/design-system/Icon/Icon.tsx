e4c05afb47b655151ae752ff705b9140135db6ad
Durdona Ibragimova <durdona.dev@gmail.com>
2024-01-25 09:51:26 -0500

    Implement Badge with icon

--- src/design-system/Icon/Icon.tsx
+++ src/design-system/Icon/Icon.tsx
@@ -4,10 +4,17 @@ import sprite from "./sprite.svg";
 type IconProps = {
     iconName: string;
     className?: string;
+    height?: string;
+    width?: string;
 };
-const Icon: React.FC<IconProps> = ({ iconName, className }) => {
+
+const Icon: React.FC<IconProps> = ({ iconName, className, height, width }) => {
     return (
-        <svg height="2.4rem" width="2.4rem" className={className || ""}>
+        <svg
+            height={height || "2.4rem"}
+            width={width || "2.4rem"}
+            className={className || ""}
+        >
             <use xlinkHref={`${sprite}#${iconName}`} />
         </svg>
     );
