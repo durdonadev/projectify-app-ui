bccf3a35389802acbf85ea823e8e1967321d8c2b
Durdona Ibragimova <durdona.dev@gmail.com>
2024-01-29 23:37:37 -0500

    Refactor Admin Projects api

--- src/api/admin-team-members.ts
+++ src/api/admin-team-members.ts
@@ -1,4 +1,4 @@
-interface createTeamMemberInput {
+interface CreateTeamMemberInput {
     firstName: string;
     lastName: string;
     position: string;
@@ -15,11 +15,10 @@ class AdminTeamMembers {
         }/team-members`;
     }
 
-    async add(input: createTeamMemberInput) {
+    async add(input: CreateTeamMemberInput) {
         try {
             const rawAuthToken = localStorage.getItem("authToken");
             const authToken = rawAuthToken ? JSON.parse(rawAuthToken) : "";
-
             const response = await fetch(`${this.url}`, {
                 method: "POST",
                 headers: {
