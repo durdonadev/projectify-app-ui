198601b247ed6a69a84b7650cb3885bb69c9b825
Durdona Ibragimova <durdona.dev@gmail.com>
2024-02-03 00:20:30 -0500

    Update admin personal task status by drag and drop

--- src/pages/admin-platform/admin-personal-tasks/AdminPersonalTasks.tsx
+++ src/pages/admin-platform/admin-personal-tasks/AdminPersonalTasks.tsx
@@ -176,7 +176,9 @@ const AdminPersonalTasks = () => {
                 };
                 dispatch(action);
             })
-            .catch((e) => {});
+            .catch((e) => {
+                toast.error("Something went wrong! Try Later.");
+            });
     };
 
     const groupedTasks = groupTasksByStatus(adminPersonalTasks);
