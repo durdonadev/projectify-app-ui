dae6e5f38fc4b167e73d36f9fca60de6080b7001
Durdona Ibragimova <durdona.dev@gmail.com>
2024-02-05 22:18:44 -0500

    Create Checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import { OptionValue, Select } from "./design-system";
+import { Checkbox } from "./design-system";
 
 const Base = styled.div`
     font-size: 1.6rem;
@@ -14,7 +14,7 @@ const Base = styled.div`
 `;
 
 const App = () => {
-    const [value, setValue] = useState<OptionValue>("");
+    const [value, setValue] = useState(false);
     return (
         <Base>
             <h1>Welcome </h1>
@@ -36,20 +36,14 @@ const App = () => {
                 Team Member Reset Password
             </Link>
             <Link to="team-member/platform">Team Member Platform</Link>
-            <Select
-                options={[
-                    { label: "Option1", value: "option1" },
-                    { label: "Option2", value: "option2" },
-                    { label: "Option3", value: "option3" },
-                    { label: "Option4", value: "option4" },
-                    { label: "Option5", value: "option5" },
-                    { label: "Option6", value: "option6" }
-                ]}
-                headerPlaceholder="Select Option"
-                onSelect={(option) => setValue(option.value)}
-                size="md"
-                shape="circle"
-                value={value}
+            <Checkbox
+                label="Hello world"
+                id="checkbox"
+                checked={value}
+                onChange={(value) => setValue(value)}
+                shape="rounded"
+                indeterminate
+                position="end"
             />
         </Base>
     );
