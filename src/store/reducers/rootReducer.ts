575380a5c2f50e365f0cc834473c550299234e17
Durdona Ibragimova <durdona.dev@gmail.com>
2024-02-12 08:04:20 -0500

    Better naming actions, reducers, types

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -3,11 +3,13 @@ import { adminTasksReducer } from "./adminTasksReducer";
 import { GlobalState } from "../state";
 import { ActionType } from "../actions";
 import { teamMemberTasksReducer } from "./teamMemberTasksReducer";
+import { adminTeamMembersReducer } from "./adminTeamMembersReducer";
 
 const rootReducer = (state: GlobalState, action: ActionType): GlobalState => {
     const newState: GlobalState = {
         user: userReducer(state.user, action),
         adminPersonalTasks: adminTasksReducer(state.adminPersonalTasks, action),
+        teamMembers: adminTeamMembersReducer(state.teamMembers, action),
         teamMemberPersonalTasks: teamMemberTasksReducer(
             state.teamMemberPersonalTasks,
             action
--- src/store/state/state.ts
+++ src/store/state/state.ts
@@ -1,16 +1,19 @@
-import { AdminUser, Task, TeamMemberUser } from "../../types";
+import { AdminUser, Task, TeamMember, TeamMemberUser } from "../../types";
 
 export type UserState = AdminUser | TeamMemberUser | null;
+export type TeamMemberState = TeamMember[];
 export type TaskState = Task[];
 
 export interface GlobalState {
     user: UserState;
     adminPersonalTasks: TaskState;
+    teamMembers: TeamMemberState;
     teamMemberPersonalTasks: TaskState;
 }
 
 export const initialState: GlobalState = {
     user: null,
     adminPersonalTasks: [],
+    teamMembers: [],
     teamMemberPersonalTasks: []
 };
