60c57cc5b61dbfa4f72693051528a05ecbc75de4
Durdona Ibragimova <durdona.dev@gmail.com>
2024-02-18 19:22:49 -0500

    Filter Team members based on status

--- src/pages/admin-platform/admin-team-members/index.tsx
+++ src/pages/admin-platform/admin-team-members/index.tsx
@@ -9,11 +9,14 @@ import toast from "react-hot-toast";
 import { PageHeader } from "../../components";
 import { TeamMemberFilters } from "./TeamMemberFilters";
 import { TeamMembersTable } from "./TeamMembersTable";
+import { Option } from "../../../design-system";
+import { TeamMemberStatus } from "../../../types";
 
 const AdminTeamMembersPage = () => {
     const [showCreateTeamMemberModal, setShowCreateTeamMemberModal] =
         useState(false);
     const [isTeamMembersFetching, setIsTeamMembersFetching] = useState(true);
+    const [statusFilter, setStatusFilter] = useState("");
     const {
         state: { teamMembers },
         dispatch
@@ -37,16 +40,26 @@ const AdminTeamMembersPage = () => {
             });
     }, []);
 
+    const handleSetStatusFilter = (filter: Option) => {
+        setStatusFilter(filter.value as TeamMemberStatus);
+    };
+
     if (isTeamMembersFetching) return null;
 
     const teamMembersArr = Object.values(teamMembers);
+    const filteredTeamMembers = teamMembersArr.filter(
+        (teamMember) =>
+            teamMember.status === statusFilter ||
+            statusFilter === "all" ||
+            statusFilter === ""
+    );
 
     return (
         <Page>
             {!teamMembersArr.length ? (
                 <NoDataPlaceholder
                     illustrationUrl={noTeamMember}
-                    text="You don’t have any team members yet!"
+                    text="You don't have any team members yet!"
                     buttonText="Add a Team Member"
                     buttonAction={() => setShowCreateTeamMemberModal(true)}
                 ></NoDataPlaceholder>
@@ -59,8 +72,11 @@ const AdminTeamMembersPage = () => {
                             setShowCreateTeamMemberModal(true)
                         }
                     />
-                    <TeamMemberFilters />
-                    <TeamMembersTable data={teamMembersArr} />
+                    <TeamMemberFilters
+                        setSelectedStatus={handleSetStatusFilter}
+                        selectedStatus={statusFilter}
+                    />
+                    <TeamMembersTable data={filteredTeamMembers} />
                 </PageContent>
             )}
             <CreateTeamMemberModal
