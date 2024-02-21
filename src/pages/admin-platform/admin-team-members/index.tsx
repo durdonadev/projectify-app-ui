fb8b3b5f9cfd87f136a340b296262f050e7a13d1
Durdona Ibragimova <durdona.dev@gmail.com>
2024-02-20 23:54:41 -0500

    Search teamMembers by text

--- src/pages/admin-platform/admin-team-members/index.tsx
+++ src/pages/admin-platform/admin-team-members/index.tsx
@@ -17,6 +17,8 @@ const AdminTeamMembersPage = () => {
         useState(false);
     const [isTeamMembersFetching, setIsTeamMembersFetching] = useState(true);
     const [statusFilter, setStatusFilter] = useState("");
+    const [searchText, setSearchText] = useState("");
+
     const {
         state: { teamMembers },
         dispatch
@@ -47,19 +49,37 @@ const AdminTeamMembersPage = () => {
     if (isTeamMembersFetching) return null;
 
     const teamMembersArr = Object.values(teamMembers);
-    const filteredTeamMembers = teamMembersArr.filter(
-        (teamMember) =>
-            teamMember.status === statusFilter ||
-            statusFilter === "all" ||
-            statusFilter === ""
-    );
+
+    const filterTeamMembers = () => {
+        let filteredTeamMembers = teamMembersArr;
+        if (statusFilter && statusFilter !== "all") {
+            filteredTeamMembers = filteredTeamMembers.filter(
+                (teamMember) => teamMember.status === statusFilter
+            );
+        }
+        if (searchText) {
+            filteredTeamMembers = filteredTeamMembers.filter(
+                (teamMember) =>
+                    teamMember.firstName
+                        .toLowerCase()
+                        .includes(searchText.toLowerCase()) ||
+                    teamMember.lastName
+                        .toLowerCase()
+                        .includes(searchText.toLowerCase())
+            );
+        }
+
+        return filteredTeamMembers;
+    };
+
+    const filteredTeamMembers = filterTeamMembers();
 
     return (
         <>
             {!teamMembersArr.length ? (
                 <NoDataPlaceholder
                     illustrationUrl={noTeamMember}
-                    text="You don't have any team members yet!"
+                    text="You don’t have any team members yet!"
                     buttonText="Add a Team Member"
                     buttonAction={() => setShowCreateTeamMemberModal(true)}
                 ></NoDataPlaceholder>
@@ -75,10 +95,13 @@ const AdminTeamMembersPage = () => {
                     <TeamMemberFilters
                         setSelectedStatus={handleSetStatusFilter}
                         selectedStatus={statusFilter}
+                        searchText={searchText}
+                        setSearchText={setSearchText}
                     />
                     <TeamMembersTable data={filteredTeamMembers} />
                 </>
             )}
+
             <CreateTeamMemberModal
                 show={showCreateTeamMemberModal}
                 closeModal={() => setShowCreateTeamMemberModal(false)}
