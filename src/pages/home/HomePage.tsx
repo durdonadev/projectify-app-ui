82e621d278ec40117573b5e22cd5be449c38d4ce
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-02 17:02:50 -0500

    Add Features Component to Home Page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { TopNavigation } from "./sections/top-navigation/TopNavigation";
 import styled from "styled-components";
-import { FeaturesCard } from "../components/FeaturesCard";
+import { Features } from "./sections/features/Features";
 
 const Base = styled.div`
     max-width: 144rem;
     margin: 0 auto;
-    font-size: 0;
-    background-color: #a3c6e4;
 `;
 
 const HomePage = () => {
     return (
         <Base>
-            <h1>Welcome to the Home Page</h1>
             <TopNavigation />
-            <FeaturesCard />
+            <Features />
         </Base>
     );
 };
