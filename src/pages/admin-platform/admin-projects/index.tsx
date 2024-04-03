f1379733eb1c9123eecc9175fe821adbc788f679
Durdona Ibragimova <durdona.dev@gmail.com>
2024-04-02 23:30:12 -0400

    Refactor filter projects

--- src/pages/admin-platform/admin-projects/index.tsx
+++ src/pages/admin-platform/admin-projects/index.tsx
@@ -15,7 +15,7 @@ import { ProjectsTable } from "./ProjectsTable";
 const AdminProjectsPage = () => {
     const [showCreateProjectModal, setShowCreateProjectModal] = useState(false);
     const [isProjectsFetching, setIsProjectsFetching] = useState(true);
-    const [statusFilter, setStatusFilter] = useState("");
+    const [statusFilter, setStatusFilter] = useState("all");
     const [searchText, setSearchText] = useState("");
     const [sortedBy, setSortedBy] = useState("");
 
@@ -54,29 +54,11 @@ const AdminProjectsPage = () => {
 
     const projectsArr = Object.values(projects);
 
-    const filterProjects = () => {
-        let filteredProjects = projectsArr;
-        if (statusFilter && statusFilter !== "all") {
-            filteredProjects = filteredProjects.filter(
-                (project) => project.status === statusFilter
-            );
-        }
-        if (searchText) {
-            filteredProjects = filteredProjects.filter(
-                (project) =>
-                    project.name
-                        .toLowerCase()
-                        .includes(searchText.toLowerCase()) ||
-                    project.description
-                        .toLowerCase()
-                        .includes(searchText.toLowerCase())
-            );
-        }
-
-        return filteredProjects;
+    const filterProjects = (value: string) => {
+        if (value === "all") return projectsArr;
+        return projectsArr.filter((project) => project.status === value);
     };
-
-    const filteredProjects = filterProjects();
+    const filteredProjects = filterProjects(statusFilter);
 
     return (
         <>
